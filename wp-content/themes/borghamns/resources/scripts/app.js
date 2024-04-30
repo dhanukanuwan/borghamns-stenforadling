f3b275d45b25466306e6551091f7a3b63a6bd781
Dhanuka Gunarathna <nuwan.dhanuka@gmail.com>
2024-04-30 15:58:03 +0200

    removing unwanted bootstrap component css and javascript

--- wp-content/themes/borghamns/resources/scripts/app.js
+++ wp-content/themes/borghamns/resources/scripts/app.js
@@ -1,5 +1,6 @@
 import domReady from '@roots/sage/client/dom-ready';
-import 'bootstrap';
+import 'bootstrap/js/dist/carousel';
+import 'bootstrap/js/dist/collapse';
 import 'lazysizes';
 import pageSearch from './includes/search';
 import mobileNav from './includes/mobileNav';
